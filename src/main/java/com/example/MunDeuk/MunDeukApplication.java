5c53f59245117514d31528003f8d80fae9ca5416
Lee seung chan <dltmdcks702@gmail.com>
2023-05-31 19:38:41 +0900

    init boot

---
+++ src/main/java/com/example/MunDeuk/MunDeukApplication.java
@@ -0,0 +1,13 @@
+package com.example.MunDeuk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MunDeukApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MunDeukApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/MunDeuk/MunDeukApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MunDeuk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MunDeukApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
